eebdd0f574983e5bc09f305d51e188e588016f28
Victor Alessander <alessander@protonmail.com>
2018-02-25 17:20:17 -0300

    Some changes to make the code more generic

--- program.py
+++ program.py
@@ -1,15 +1,16 @@
 import face_recognition
 from PIL import Image, ImageDraw, ImageFont
+import sys
 
 
-def main():
+def main(known_person, known_image_file):
     try:
         # Load known image
-        known_image = face_recognition.load_image_file("Michael.jpg")
+        known_image = face_recognition.load_image_file(known_image_file)
         known_image_encoding = face_recognition.face_encodings(known_image)[0]
 
         # Load unknown image
-        unknown_image = face_recognition.load_image_file("desconhecidos.jpg")
+        unknown_image = face_recognition.load_image_file("unknown.jpg")
         #face_locations = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model="cnn")
         unknown_face_locations = face_recognition.face_locations(unknown_image)
         unknown_face_encoding = face_recognition.face_encodings(unknown_image, unknown_face_locations)
@@ -19,13 +20,13 @@ def main():
         raise e
 
     known_faces_encodings = [known_image_encoding]
-    known_faces_names = ["Michael"]
+    known_faces_names = [known_person.capitalize()]
 
     pil_image = Image.fromarray(unknown_image)
     draw = ImageDraw.Draw(pil_image)
 
     if unknown_face_locations:
-        print("[!] Detected some faces")
+        print("[+] Detected some faces")
 
         for (top, right, bottom, left), face_encoding in zip(unknown_face_locations, unknown_face_encoding):
             matches = face_recognition.compare_faces(known_faces_encodings, face_encoding)
@@ -35,7 +36,7 @@ def main():
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_faces_names[first_match_index]
-                print("[!] Found known face: {}".format(name))
+                print("[+] Found known face: {}".format(name))
 
             font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 15)
             width, height = pil_image.size
@@ -54,4 +55,6 @@ def main():
     #pil_image.show()
 
 if __name__ == "__main__":
-    main()
+    person = sys.argv[1]
+    known_person_file = sys.argv[2]
+    main(person, known_person_file)
